6c6641f983c697254bc084942995dcc293fd697c
Shaffaq Hai <hai.shaffaq@gmail.com>
2025-11-27 16:31:56 -0500

    added by city

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -21,7 +21,26 @@ const weatherAPI = {
 
 
 
-    }
+    },
+    getCity : async  (weather) =>  {
+        try{
+            const response = await axiosInstance.get('/weather', {
+             params: {
+            q: weather,
+            appid: "ad1ef316b4506bcd3e033d407b9d004e"
+        }
+    })
+
+        console.log(response.data);
+        return response.data
+
+        }catch(error){
+           throw new Error("Error fetching : " + error.message)
+        }
+
+
+
+    },
    
 }
 
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -3,6 +3,7 @@ import weatherAPI from "../api/weatherAPI";
 
 function ViewWeather(){
     const[details, setDetails] = useState(null)
+    const [city,setCity] = useState("")
     const getWeatherDetails = async () => {
 
          try{
@@ -16,20 +17,66 @@ function ViewWeather(){
 
 
     }
+      const getWeatherByCity = async (weather) => {
+
+         try{
+            const weatherDetails = await weatherAPI.getCity(weather)
+            setDetails(weatherDetails)
+            console.log("Weather Details: ", weatherDetails)
+
+        }catch(error){
+            console.error("Failed to fetch weather details: ", error)
+        }
+
+
+    }
+     const handleSubmit = (e) => {
+        e.preventDefault()
+        weatherAPI.getCity(city)
+        .then(response => {
+            setDetails(response)
+        })
+        .catch(error =>{
+            console.error("there was an error changing city ", error)
+        })
+
+    }
+ 
 
     useEffect(()=>{
         getWeatherDetails()
-    },[])
+    },[city])
     if (!details) {
     return <div>Loading weather...</div>;
   }
     return(
         <div>
+            <h1>{details.name}</h1>
             <div>
-             <p style={{ textTransform: "capitalize" }}>
-                {details.weather[0].description}
-            </p>
-             </div>  
+            <p > longitude: {details.coord.lon} </p>
+             <p >latitude: {details.coord.lat} </p>
+            </div> 
+            <div>
+                 <p >{details.weather[0].description}</p>
+                <p >{details.weather[0].main}</p>
+            </div>
+            <div>
+                 <p >Pressure: {details.main.pressure}</p>
+                 <p >Humidty: {details.main.humidity}</p>
+                 <p >Wind: {details.main.pressure}</p>
+            </div>
+
+            <div>
+                <form onSubmit={handleSubmit}>
+                <div>
+                  <label>city: </label>
+                  <input type="text" name="city" value= {city} onChange={(e) => setCity(e.target.value)} />
+                  
+                 </div>     
+
+                <button type="submit">Change City</button>    
+                </form>
+            </div>
         
         </div>
     )
